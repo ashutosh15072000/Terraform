3d8fed44486ec34dcb77c7e14c981c657b42ea7e
Ashutosh singh <ashutoshs019@gmail.com>
2025-08-06 16:54:17 +0530

    terraform-gcp

---
+++ terraform-gcp-demo/main.tf
@@ -0,0 +1,28 @@
+provider "google" {
+    credentials = file("gcp-account.json")
+    project = "github-actions-468107"
+    region = "europe-west4"
+    zone = "europe-west4-a"
+}
+
+
+resource "google_compute_instance" "default" {
+  name         = "test"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+    }
+  }
+
+  # To keep the setup simple you can set the network_interface to default
+  # For Advance network setup refer to Point-7 : Setup Network and Firewall for virtual machine
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+}
